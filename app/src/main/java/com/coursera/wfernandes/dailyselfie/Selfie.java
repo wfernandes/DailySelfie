f5cf79272be9790ee485bb34712ac9d8f897058b
Warren Fernandes <warren.f.fernandes@gmail.com>
2014-11-25 13:22:51 -0700

    Daily Selfie Project - Adds Selfie bitmap thumbnail to the Selfie class

--- app/src/main/java/com/coursera/wfernandes/dailyselfie/Selfie.java
+++ app/src/main/java/com/coursera/wfernandes/dailyselfie/Selfie.java
@@ -1,27 +1,28 @@
 package com.coursera.wfernandes.dailyselfie;
 
+import android.graphics.Bitmap;
+
 public class Selfie {
 
     private String selfieName;
+    private Bitmap selfieThumb;
     private String selfiePath;
 
-    public Selfie(String name){
+    public Selfie(String name, String fullFilePath, Bitmap thumbnail) {
         this.selfieName = name;
+        this.selfiePath = fullFilePath;
+        this.selfieThumb = thumbnail;
     }
 
     public String getSelfieName() {
         return selfieName;
     }
 
-    public void setSelfieName(String selfieName) {
-        this.selfieName = selfieName;
-    }
-
     public String getSelfiePath() {
         return selfiePath;
     }
 
-    public void setSelfiePath(String selfiePath) {
-        this.selfiePath = selfiePath;
+    public Bitmap getSelfieThumb() {
+        return selfieThumb;
     }
 }
